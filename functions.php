5bc07c7523a1db535d9600b821478ec309346b66
stephen harris <subliminalvisions@gmail.com>
2021-05-12 12:24:10 -0600

    browser string to body class

--- functions.php
+++ functions.php
@@ -35,6 +35,23 @@ return $classes;
 }
 add_filter('body_class','add_category_body');
 
+// add browser string to body_class
+add_filter('body_class','browser_body_class');
+function browser_body_class($classes) {
+  global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+
+  if($is_lynx) $classes[] = 'lynx';
+  elseif($is_gecko) $classes[] = 'gecko';
+  elseif($is_opera) $classes[] = 'opera';
+  elseif($is_NS4) $classes[] = 'ns4';
+  elseif($is_safari) $classes[] = 'safari';
+  elseif($is_chrome) $classes[] = 'chrome';
+  elseif($is_IE) $classes[] = 'ie';
+  else $classes[] = 'unknown';
+
+  if($is_iphone) $classes[] = 'iphone';
+  return $classes;
+}
 
 // Custom Widget Sidebar
 function stephenz_widgets_init() {
